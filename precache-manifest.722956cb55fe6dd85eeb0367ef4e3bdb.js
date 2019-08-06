06e1196ef040f8ea486227a2b4d2b112b94585d7
KuaqSon <qson.ng@gmail.com>
2019-08-06 20:35:11 +0700

    Updates

---
+++ precache-manifest.722956cb55fe6dd85eeb0367ef4e3bdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8494b049bbfef313c2fc2862d3fc4d",
+    "url": "/instadolo/index.html"
+  },
+  {
+    "revision": "a2bb5a018f5dd727885b",
+    "url": "/instadolo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/instadolo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a2bb5a018f5dd727885b",
+    "url": "/instadolo/static/js/main.f0b78bd9.chunk.js"
+  },
+  {
+    "revision": "e6548080053614d52ece",
+    "url": "/instadolo/static/js/runtime~main.0d2c3ed3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/instadolo/static/media/logo.5d5d9eef.svg"
+  }
+]);
