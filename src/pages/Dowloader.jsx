893a083dce89ddb6a03824c5e225d2eb66ca498b
KuaqSon <qson.ng@gmail.com>
2019-08-07 03:44:51 +0700

    Added image card

--- src/pages/Dowloader.jsx
+++ src/pages/Dowloader.jsx
@@ -1,22 +1,85 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./Downloader.css";
+
+const enpoint = "https://moneyfluapi.herokuapp.com/insta/image-urls";
+const gradientBackgroundTypes = [
+  "normal",
+  "water",
+  "electric",
+  "fire",
+  "psychic",
+  "dark",
+  "grass",
+  "ice",
+  "fairy"
+];
 
 class Downloader extends Component {
   state = {
-    link: ""
+    link: "",
+    result_url: "",
+    post_title: ""
   };
 
   downloadImage = () => {
+    const self = this;
     const { link } = this.state;
 
-    if (!link) return;
+    if (!link) {
+      toast.warning("Invalid post url!", {
+        autoClose: 1000
+      });
+      return;
+    }
 
+    axios
+      .post(enpoint, {
+        post_url: link
+      })
+      .then(function(response) {
+        const { data } = response;
+
+        if (!data) {
+          toast.error("Failed!");
+        }
+
+        const {
+          isError,
+          message,
+          resp: { src_url, title }
+        } = data;
+
+        if (!isError) {
+          toast("Yayyy, Get image successfully!");
+
+          self.setState({
+            result_url: src_url,
+            post_title: title
+          });
+        } else {
+          toast.error(message || "Failed!");
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   };
 
   render() {
-    const { link } = this.state;
+    const { link, result_url, post_title } = this.state;
+    const cardClassName = `card card--${
+      gradientBackgroundTypes[Math.floor(Math.random() * 9)]
+    }`;
+    console.log("result_url", result_url);
     return (
       <div className="downloader-container">
+        <div className="intro">
+          <div className="app-name">Instagram downloader</div>
+          <div className="author">By: Quang Son Nguyen</div>
+        </div>
         <div className="link-panel">
           <div className="link-input">
             <label className="field a-field a-field_a3 page__field">
@@ -31,13 +94,44 @@ class Downloader extends Component {
               </span>
             </label>
           </div>
-
           <div className="download-buttons">
             <button className="btt" onClick={() => this.downloadImage()}>
-              Download
+              Load Image
             </button>
           </div>
         </div>
+        <ToastContainer
+            autoClose={1500}
+            position="top-center"
+            hideProgressBar={false}
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnVisibilityChange={false}
+            draggable
+            pauseOnHover={false}
+          />
+        {result_url && (
+          // <div
+          //   className="image-panel"
+          //   style={{ backgroundImage: `url(${result_url})` }}
+          // />
+
+          <div className={cardClassName}>
+            <div
+              className="card__image-container"
+              style={{ backgroundImage: `url(${result_url})` }}
+            />
+            <div className="card__caption">
+              <div>{post_title}</div>
+              <div className="card__type">
+                <a href={result_url} target="_blank" className="cir-btn">
+                  Download
+                </a>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
