0eaed0327c4a8ec69745863bb1deecac7a8a0570
KuaqSon <qson.ng@gmail.com>
2019-08-09 00:15:49 +0700

    catch download error

--- src/pages/Dowloader.jsx
+++ src/pages/Dowloader.jsx
@@ -93,18 +93,22 @@ class Downloader extends Component {
 
     const fileName = tit + (url.indexOf(".mp4") > 0 ? ".mp4" : ".jpg");
 
-    axios({
-      url: url,
-      method: "GET",
-      responseType: "blob" // important
-    }).then(response => {
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", fileName);
-      document.body.appendChild(link);
-      link.click();
-    });
+    try {
+      axios({
+        url: url,
+        method: "GET",
+        responseType: "blob" // important
+      }).then(response => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", fileName);
+        document.body.appendChild(link);
+        link.click();
+      });
+    } catch (e) {
+      window.open(url, "_blank");
+    }
   };
 
   render() {
